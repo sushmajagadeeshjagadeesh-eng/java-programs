6a99727ab0b7292fa70ca97d9bd5a3c4d59d352f
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 04:03:18 -0700

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,10 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 5, b = 10, temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
