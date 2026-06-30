22a7223bd9bfc3cad04f85f3202d685e139a4a76
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 04:02:02 -0700

    Add files via upload

---
+++ even odd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 8;
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
