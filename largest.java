c99d61e7f817488de878bd391a427a3efc74c8a6
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 04:04:06 -0700

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,9 @@
+public class Largest {
+    public static void main(String[] args) {
+        int a = 15, b = 25;
+        if (a > b)
+            System.out.println(a + " is largest");
+        else
+            System.out.println(b + " is largest");
+    }
+}
