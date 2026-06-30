c0b588ef11cc84a83dd6918620455ca699b36bde
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 09:13:14 -0700

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,18 @@
+public class Prime {
+    public static void main(String[] args) {
+        int n = 13;
+        boolean prime = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime");
+    }
+}
