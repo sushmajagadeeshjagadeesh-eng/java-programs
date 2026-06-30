4f910944cd1137cae2e4858845e11348d564ab1b
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 08:55:40 -0700

    Add files via upload

---
+++ Largest of Three numbers.java
@@ -0,0 +1,12 @@
+public class LargestThree {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 15;
+
+        if (a >= b && a >= c)
+            System.out.println(a + " is largest");
+        else if (b >= c)
+            System.out.println(b + " is largest");
+        else
+            System.out.println(c + " is largest");
+    }
+}
