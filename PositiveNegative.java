e7f1aad4eb95a19c4bf8828145597b5094810791
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 08:56:27 -0700

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,9 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        int n = -5;
+        if (n >= 0)
+            System.out.println("Positive");
+        else
+            System.out.println("Negative");
+    }
+}
