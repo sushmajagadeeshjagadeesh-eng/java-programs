4647f86d7802694ec1890a8aeb061b85b380079b
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-06-30 09:11:10 -0700

    Add files via upload

---
+++ MaxArray.java
@@ -0,0 +1,13 @@
+public class MaxArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 7, 40, 18};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        System.out.println("Maximum = " + max);
+    }
+}
