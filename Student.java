3bfea83a1222b80076274824e161539c2311b065
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-07-01 05:57:32 -0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name = "Sushma";
+    int age = 20;
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.name);
+        System.out.println(s.age);
+    }
+}
