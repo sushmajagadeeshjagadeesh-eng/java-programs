75ca09ccc993ad79cfb984bbf23ca41d83ca898f
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-07-01 05:59:53 -0700

    Add files via upload

---
+++ MessageMethod.java
@@ -0,0 +1,9 @@
+public class MessageMethod {
+    static void display() {
+        System.out.println("Welcome to Java");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
