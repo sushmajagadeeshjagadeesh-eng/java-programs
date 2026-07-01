2c47b5aec0efc5a74eb25f7c9a803f6e7d6344d5
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-07-01 05:59:06 -0700

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,10 @@
+public class Mobile {
+    String model = "Samsung";
+    int price = 20000;
+
+    public static void main(String[] args) {
+        Mobile m = new Mobile();
+        System.out.println(m.model);
+        System.out.println(m.price);
+    }
+}
