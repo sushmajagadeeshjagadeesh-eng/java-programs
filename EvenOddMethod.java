c64496fdf73774b71dc8321065a5bd8a6679ee11
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-07-01 05:56:47 -0700

    Add files via upload

---
+++ EvenOddMethod.java
@@ -0,0 +1,12 @@
+public class EvenOddMethod {
+    static void check(int n) {
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+
+    public static void main(String[] args) {
+        check(9);
+    }
+}
