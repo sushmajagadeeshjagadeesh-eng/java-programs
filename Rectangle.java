3636aa58d72475a7a2823d904253651e429fc43b
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-07-01 05:58:45 -0700

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle {
+    int length = 10;
+    int width = 5;
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        System.out.println("Area = " + (r.length * r.width));
+    }
+}
