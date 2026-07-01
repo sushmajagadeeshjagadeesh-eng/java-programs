eb0b3836803683ffd5f4361117328e8f70672a00
sushmajagadeeshjagadeesh-eng <sushmajagadeeshjagadeesh@gmail.com>
2026-07-01 05:58:27 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    int id = 101;
+    String name = "Ravi";
+
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        System.out.println(e.id);
+        System.out.println(e.name);
+    }
+}
